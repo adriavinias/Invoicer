54f6dae85208f2923c13ddbc743ebb9e2857cffb
adriavinias <adria.vinas.pascual@gmail.com>
2024-02-06 20:15:49 +0100

    test

--- app/components/LogOutButton.tsx
+++ app/components/LogOutButton.tsx
@@ -3,6 +3,7 @@ import { signOut } from "next-auth/react";
 export default function SignOutButton({children}){
     return(
         <button
+        className="flex"
         onClick={()=>signOut()}>
             {children}
         </button>
